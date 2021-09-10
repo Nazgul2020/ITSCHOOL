1e64d7150db018d96e7315e6cd7dafadd6092e9b
Nazgul <aset_76@mail.ru>
2021-09-10 22:46:31 +0600

    Практическая работа 1

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,17 @@
-const numberOfFilms= 5 ;
+const numberOfFilms= +prompt ('Сколько фильмов вы уже просмтрели?', '' ) ;
+const personalMovieDB = {
+    count:numberOfFilms,
+    movies:{},
+    actors: {},
+    genres: [],
+    privat: false
+ };
+
+ const a = prompt ("Один из последних просмотренных фидьм", ''),
+       b = prompt ("На сколько его оцените?", ''),
+       c = prompt ("Один из последних просмотренных фидьм", ''),
+       d = prompt ("На сколько его оцените?", '');
+
+    personalMovieDB.movies[a]=b;
+    personalMovieDB.movies[d]=c;
+    console.log(personalMovieDB);
